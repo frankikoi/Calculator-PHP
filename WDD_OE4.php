b062cfe1ee776f651d52ac9a5f18304e57381b7d
Azel Opeña <azel.opena22@gmail.com>
2021-10-24 10:58:41 +0800

    First Commit

---
+++ WDD_OE4.php
@@ -0,0 +1,293 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class="supernova"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="alternate" type="application/json+oembed" href="https://www.jotform.com/oembed/?format=json&amp;url=https%3A%2F%2Fform.jotform.com%2F212958554367468" title="oEmbed Form">
+<link rel="alternate" type="text/xml+oembed" href="https://www.jotform.com/oembed/?format=xml&amp;url=https%3A%2F%2Fform.jotform.com%2F212958554367468" title="oEmbed Form">
+<meta property="og:title" content="Find the Highest Value" >
+<meta property="og:url" content="https://form.jotform.com/212958554367468" >
+<meta property="og:description" content="Please click the link to complete this form." >
+<meta name="slack-app-id" content="AHNMASS8M">
+<link rel="shortcut icon" href="https://cdn.jotfor.ms/assets/img/favicons/favicon-2021.svg">
+<link rel="canonical" href="https://form.jotform.com/212958554367468" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=1" />
+<meta name="HandheldFriendly" content="true" />
+<title>Find the Highest Value</title>
+<link href="https://cdn01.jotfor.ms/static/formCss.css?3.3.28645" rel="stylesheet" type="text/css" />
+<style type="text/css">@media print{.form-section{display:inline!important}.form-pagebreak{display:none!important}.form-section-closed{height:auto!important}.page-section{position:initial!important}}</style>
+<link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/css/styles/nova.css?3.3.28645" />
+<link type="text/css" rel="stylesheet" href="https://cdn03.jotfor.ms/themes/CSS/566a91c2977cdfcd478b4567.css?themeRevisionID=5f6c4c83346ec05354558fe8"/>
+<link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/css/styles/payment/payment_feature.css?3.3.28645" />
+<style type="text/css">
+    .form-label-left{
+        width:150px;
+    }
+    .form-line{
+        padding-top:12px;
+        padding-bottom:12px;
+    }
+    .form-label-right{
+        width:150px;
+    }
+    body, html{
+        margin:0;
+        padding:0;
+        background:#fff;
+    }
+
+    .form-all{
+        margin:0px auto;
+        padding-top:0px;
+        width:690px;
+        color:#555 !important;
+        font-family:"Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Verdana, sans-serif;
+        font-size:14px;
+    }
+    .form-radio-item label, .form-checkbox-item label, .form-grading-label, .form-header{
+        color: #555;
+    }
+
+</style>
+
+<style type="text/css" id="form-designer-style">
+    /* Injected CSS Code */
+/*PREFERENCES STYLE*/
+    .form-all {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-all .qq-upload-button,
+    .form-all .form-submit-button,
+    .form-all .form-submit-reset,
+    .form-all .form-submit-print {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-all .form-pagebreak-back-container,
+    .form-all .form-pagebreak-next-container {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-header-group {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-label {
+      font-family: Lucida Grande, sans-serif;
+    }
+  
+    .form-label.form-label-auto {
+      
+    display: inline-block;
+    float: left;
+    text-align: left;
+  
+    }
+  
+    .form-line {
+      margin-top: 12px;
+      margin-bottom: 12px;
+    }
+  
+    .form-all {
+      max-width: 690px;
+      width: 100%;
+    }
+  
+    .form-label.form-label-left,
+    .form-label.form-label-right,
+    .form-label.form-label-left.form-label-auto,
+    .form-label.form-label-right.form-label-auto {
+      width: 160px;
+    }
+  
+    .form-all {
+      font-size: 14px
+    }
+    .form-all .qq-upload-button,
+    .form-all .qq-upload-button,
+    .form-all .form-submit-button,
+    .form-all .form-submit-reset,
+    .form-all .form-submit-print {
+      font-size: 14px
+    }
+    .form-all .form-pagebreak-back-container,
+    .form-all .form-pagebreak-next-container {
+      font-size: 14px
+    }
+  
+    .supernova .form-all, .form-all {
+      background-color: ;
+    }
+  
+    .form-all {
+      color: #555;
+    }
+    .form-header-group .form-header {
+      color: #555;
+    }
+    .form-header-group .form-subHeader {
+      color: #555;
+    }
+    .form-label-top,
+    .form-label-left,
+    .form-label-right,
+    .form-html,
+    .form-checkbox-item label,
+    .form-radio-item label {
+      color: #555;
+    }
+    .form-sub-label {
+      color: #6f6f6f;
+    }
+  
+    .supernova {
+      background-color: undefined;
+    }
+    .supernova body {
+      background: transparent;
+    }
+  
+    .form-textbox,
+    .form-textarea,
+    .form-dropdown,
+    .form-radio-other-input,
+    .form-checkbox-other-input,
+    .form-captcha input,
+    .form-spinner input {
+      background-color: undefined;
+    }
+  
+    .supernova {
+      background-image: none;
+    }
+    #stage {
+      background-image: none;
+    }
+  
+    .form-all {
+      background-image: none;
+    }
+  
+  .ie-8 .form-all:before { display: none; }
+  .ie-8 {
+    margin-top: auto;
+    margin-top: initial;
+  }
+  
+  /*PREFERENCES STYLE*//*__INSPECT_SEPERATOR__*/
+    /* Injected CSS Code */
+</style>
+
+<script src="https://cdn02.jotfor.ms/static/prototype.forms.js" type="text/javascript"></script>
+<script src="https://cdn03.jotfor.ms/static/jotform.forms.js?3.3.28645" type="text/javascript"></script>
+<script src="https://cdn01.jotfor.ms/js/vendor/math-processor.js?v=3.3.28645" type="text/javascript"></script>
+<script type="text/javascript">	JotForm.newDefaultTheme = false;
+	JotForm.extendsNewTheme = false;
+	JotForm.newPaymentUIForNewCreatedForms = false;
+
+   JotForm.setCalculations([{"decimalPlaces":"4","equation":"[|*|max|*|({4},{6},{5})]","insertAsText":"","newCalculationType":"1","operands":"4,6,5","readOnly":"1","resultField":"1","showBeforeInput":"","showEmptyDecimals":""}]);
+	JotForm.init(function(){
+	/*INIT-START*/
+if (window.JotForm && JotForm.accessible) $('input_4').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_5').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_6').setAttribute('tabindex',0);
+	/*INIT-END*/
+	});
+
+   JotForm.prepareCalculationsOnTheFly([null,{"name":"result","qid":"1","subLabel":"max(x,y,z)","text":"Result","type":"control_calculation"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"clickTo","qid":"3","text":"Find the Highest Value","type":"control_head"},{"name":"enterFirst","qid":"4","text":"Enter first Number (x)","type":"control_textbox"},{"name":"enterSecond","qid":"5","text":"Enter second Number (y)","type":"control_textbox"},{"name":"enterThird","qid":"6","text":"Enter third Number (z)","type":"control_textbox"}]);
+   setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,{"name":"result","qid":"1","subLabel":"max(x,y,z)","text":"Result","type":"control_calculation"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"clickTo","qid":"3","text":"Find the Highest Value","type":"control_head"},{"name":"enterFirst","qid":"4","text":"Enter first Number (x)","type":"control_textbox"},{"name":"enterSecond","qid":"5","text":"Enter second Number (y)","type":"control_textbox"},{"name":"enterThird","qid":"6","text":"Enter third Number (z)","type":"control_textbox"}]);}, 20); 
+</script>
+</head>
+<body>
+  <input type="hidden" name="formID" value="212958554367468" />
+  <input type="hidden" id="JWTContainer" value="" />
+  <input type="hidden" id="cardinalOrderNumber" value="" />
+  <div role="main" class="form-all">
+    <ul class="form-section page-section">
+      <li id="cid_3" class="form-input-wide" data-type="control_head">
+        <div class="form-header-group  header-default">
+          <div class="header-text httal htvam">
+            <h2 id="header_3" class="form-header" data-component="header">
+              Calculator
+            </h2>
+            <div id="subHeader_3" class="form-subHeader">
+              Please enter 2 value to solve a arithmetic problem
+            </div>
+          </div>
+        </div>
+      </li>
+      <form  method = "POST">
+  Enter First Number  <input type ="number" name = "x"><br>
+  Enter Second Number <input type = "number" name = "y"><br>
+  <input type = "submit" name = "add" value = "ADDITION">
+  <input type = "submit" name = "sub" value = "SUBTRACTION">
+  <input type = "submit" name = "multiply" value = "MULTIPLICATION">
+  <input type = "submit" name = "divide" value = "DIVISION">
+</form>
+<?php 
+  function add($x, $y){
+      $sum = $x + $y;
+      echo "Sum is equal to $sum";
+
+  }
+  function sub($x, $y){
+      $diff = $x - $y;
+      echo "Difference is equal to $diff";
+  }
+  function multiply ($x, $y){
+      $product = $x * $y;
+      echo "Product is equal to $product";
+  }
+  function divide ($x, $y){
+      $quotient = $x / $y;
+      echo "Quotient is equal to $quotient";
+  }
+
+  if(isset($_POST['add'])){
+      add($_POST["x"], $_POST["y"]);
+  }
+  
+  if(isset($_POST['sub'])){
+      sub($_POST["x"], $_POST["y"]);
+  }
+  if (isset($_POST['multiply'])){
+      multiply($_POST["x"], $_POST["y"]);
+  }
+  if (isset($_POST['divide'])){
+      divide($_POST["x"], $_POST["y"]);
+  }
+
+?>
+      <li style="display:none">
+        Should be Empty:
+        <input type="text" name="website" value="" />
+      </li>
+    </ul>
+  </div>
+  <script>
+  JotForm.showJotFormPowered = "new_footer";
+  </script>
+  <script>
+  JotForm.poweredByText = "Powered by Jotform";
+  </script>
+  <input type="hidden" class="simple_spc" id="simple_spc" name="simple_spc" value="212958554367468" />
+  <script type="text/javascript">
+  var all_spc = document.querySelectorAll("form[id='212958554367468'] .si" + "mple" + "_spc");
+for (var i = 0; i < all_spc.length; i++)
+{
+  all_spc[i].value = "212958554367468-212958554367468";
+}
+  </script>
+  <div class="formFooter-heightMask">
+  </div>
+  <div class="formFooter f6 branding21">
+    <div class="formFooter-wrapper formFooter-leftSide">
+      <a href="https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=212958554367468&utm_content=jotform_logo&utm_campaign=powered_by_jotform_le" target="_blank" class="formFooter-logoLink"><img class="formFooter-logo" src="https://cdn.jotfor.ms/assets/img/logo2021/jotform-logo-white.svg" alt="Jotform Logo" style="height: 44px;"></a>
+    </div>
+    <div class="formFooter-wrapper formFooter-rightSide">
+      <span class="formFooter-text">
+        Now create your own Jotform - It's free!
+      </span>
+      <a class="formFooter-button" href="https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=212958554367468&utm_content=jotform_button&utm_campaign=powered_by_jotform_le" target="_blank">Create your own Jotform</a>
+    </div>
+  </div>
+</form></body>
+</html>
+<script type="text/javascript">JotForm.ownerView=true;</script>
